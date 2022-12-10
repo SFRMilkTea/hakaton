f82901555e1cff8f56a3afc22fa74faf30d9b53d
SFRMilkTea <bayliz@bk.ru>
2022-12-10 12:10:06 +0700

    add user

---
+++ src/main/java/com/example/hakaton/entity/UserEntity.java
@@ -0,0 +1,91 @@
+package com.example.hakaton.entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String firstName;
+    private String patronymic;
+    @Column(nullable = false)
+    private String address;
+    private String pictureUrl;
+    private int bonuses;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public int getBonuses() {
+        return bonuses;
+    }
+
+    public void setBonuses(int bonuses) {
+        this.bonuses = bonuses;
+    }
+}
---
+++ src/main/java/com/example/hakaton/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.hakaton.repository;
+
+import com.example.hakaton.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+   UserEntity findByFirstName (String firstName);
+}
